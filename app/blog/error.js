e8c308b3476ec83e1c455de2440042d9cd10ccca
anthonycifuentes <anthonycifu@gmail.com>
2023-07-13 21:58:45 -0600

    add error page

---
+++ app/blog/error.js
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+
+const error = ({ error, reset }) => {
+  React.useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+};
+
+export default error;
